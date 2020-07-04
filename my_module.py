2a9c3b4a2e5c5b205c084a0b8173d07c75a5fd46
trongtinvd <trongtinvd@gmail.com>
2020-07-04 16:03:29 +0700

    hoàn thành tính năng dán nhãn từ và dán nhãn thực thể

--- my_module.py
+++ my_module.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import re
 import pandas as pd
-from tkinter import filedialog, messagebox
+from tkinter import filedialog, messagebox, Menu
 
 class PhanMemTachTu():
 	def __init__(self, root):
@@ -9,8 +9,8 @@ class PhanMemTachTu():
 
 		self.tach_cau_button = tk.Button(self.root, text='Tách câu', command=self.tach_cau)
 		self.tach_tu_button = tk.Button(self.root, text='Tách từ', command=self.tach_tu)
-		self.gan_nhan_tu_button = tk.Button(self.root, text='Gán nhãn từ')
-		self.gan_thuc_the_button = tk.Button(self.root, text='Gán thực thể')
+		self.gan_nhan_tu_button = tk.Button(self.root, text='Gán nhãn từ', command=self.gan_nhan_tu)
+		self.gan_thuc_the_button = tk.Button(self.root, text='Gán thực thể', command=self.gan_thuc_the)
 
 	def pack(self):
 		self.tach_cau_button.pack()
@@ -35,6 +35,25 @@ class PhanMemTachTu():
 		top_level.grab_set()
 		top_level.wait_window()
 		top_level.grab_release()
+
+	def gan_nhan_tu(self):
+		top_level = tk.Toplevel(self.root)
+		dan_nhan_tu_window = GanNhanTu(top_level)
+		dan_nhan_tu_window.pack()
+
+		top_level.grab_set()
+		top_level.wait_window()
+		top_level.grab_release()
+
+	def gan_thuc_the(self):
+		top_level = tk.Toplevel(self.root)
+		dan_nhan_tu_window = GanThucThe(top_level)
+		dan_nhan_tu_window.pack()
+
+		top_level.grab_set()
+		top_level.wait_window()
+		top_level.grab_release()
+
 
 
 
@@ -73,7 +92,7 @@ class TachCau():
 
 
 	def auto_segmentation(self):
-		sentences = VNSegmentation.paragraph_to_sentences(self.input.get('1.0', tk.END))
+		sentences = LanguageTool.paragraph_to_sentences(self.input.get('1.0', tk.END))
 		text = '\n\n'.join(sentences)
 		self.output.delete('1.0', tk.END)
 		self.output.insert('1.0', text)
@@ -120,9 +139,9 @@ class TachTu():
 				file.write(self.output.get('1.0', tk.END))
 
 	def auto_segmentation(self):
-		words = VNSegmentation.sentence_to_words(self.input.get('1.0', tk.END))
+		words = LanguageTool.sentence_to_words(self.input.get('1.0', tk.END))
 		text = ' '.join(words)
-		self.add_or_replace_text(self.output, text)
+		self.add_text(self.output, text)
 
 	def get_selected_text(self, event):
 		if self.input.tag_ranges("sel"):
@@ -135,9 +154,9 @@ class TachTu():
 		if text == '':
 			messagebox.showerror(parent=self.root, title='Lỗi dữ liệu đầu vào', message='Từ thêm vào không được phép rỗng.')
 		else:
-			self.add_or_replace_text(self.output, text.replace(' ', '_'))
+			self.add_text(self.output, text.replace(' ', '_'))
 
-	def add_or_replace_text(self, widget, text):
+	def add_text(self, widget, text):
 		return_val = messagebox.askyesnocancel(parent=self.root, message='-nhấn yes để thêm từ mới vào cuối văn bản\n-nhấn no để thay văn bản cũ bằng từ mới', default='yes')
 		if return_val == True:
 			widget.insert(tk.END, text + ' ')
@@ -145,7 +164,7 @@ class TachTu():
 			widget.delete('1.0', tk.END)
 			widget.insert(tk.END, text + ' ')
 
-		self.update_output_background()
+		# self.update_output_background()
 
 	def update_output_background(self):
 		self.output.tag_remove('yellow_background', '1.0', tk.END)
@@ -156,7 +175,48 @@ class TachTu():
 			self.output.highlight_pattern(word, 'yellow_background')
 
 
-class VNSegmentation():
+class LanguageTool():
+
+	pos = [
+		('Aa', 'Quality Adjectives'),
+		('An', 'Quantity Adjectives'),
+		('Cm', 'Prepositions'),
+		('Cp', 'Parallel Conjunctions'),
+		('Cs', 'Subordinating Conjunctions'),
+		('D', 'Directional co-verb'),
+		('E', 'Emotion Words'),
+		('FW', 'Foreign Words'),
+		('ID', 'Idioms'),
+		('M', 'Modifiers'),
+		('Nc', 'Countable Nouns'),
+		('Nn', 'Common Nouns'),
+		('Nq', 'Numerals'),
+		('Nr', 'Proper Nouns'),
+		('Nt', 'Temporal Nouns'),
+		('Nu', 'Concrete Nouns'),
+		('ON', 'Onomatopoeia'),
+		('Pd', 'Demonstrative Pronouns'),
+		('Pp', 'Personal Pronouns'),
+		('R', 'Adverbs'),
+		('Vc', 'Comparative Verbs'),
+		('Vd', 'Directional Verbs'),
+		('Ve', 'State Verbs'),
+		('Vv', 'Volatile Verbs')
+	]
+
+	ne = [
+		('NUM', 'Number'),
+		('PER', 'Person'),
+		('LOC', 'Location'),
+		('DTM', 'Date time'),
+		('ORG', 'Organization'),
+		('MEA', 'Measurement'),
+		('TTL', 'Title'),
+		('DES', 'Designation'),
+		('BRN', 'Brand'),
+		('ABB', 'Abbreviation'),
+		('TRM', 'Terminology')
+	]
 
 	@staticmethod
 	def paragraph_to_sentences(text):
@@ -171,10 +231,10 @@ class VNSegmentation():
 
 	@staticmethod
 	def sentence_to_words(text, max_word_len=5):
-		if not 'VNSegmentation.VN_dict' in locals():
-			VNSegmentation.create_dictionary()
+		if not 'LanguageTool.VN_dict' in locals():
+			LanguageTool.create_dictionary()
 
-		syllables = VNSegmentation.sentence_to_syllables(text.strip())
+		syllables = LanguageTool.sentence_to_syllables(text.strip())
 		result = []
 		i = 0
 		while i < len(syllables):
@@ -183,7 +243,7 @@ class VNSegmentation():
 				if j - i > max_word_len and max_word_len > 0:
 					break;
 
-				if ' '.join(syllables[i:j]) in VNSegmentation.VN_dict:
+				if ' '.join(syllables[i:j]) in LanguageTool.VN_dict:
 					a_correct_word = ' '.join(syllables[i:j])
 					new_i = j
 			if a_correct_word == '':
@@ -199,12 +259,28 @@ class VNSegmentation():
 	def create_dictionary():
 		df = pd.read_csv('VDic_uni.csv', sep=';', header=None)
 		VN_words = list(df[0])
-		VNSegmentation.VN_dict = VNSegmentation.list_to_dict(VN_words)
+		LanguageTool.VN_dict = LanguageTool.list_to_dict(VN_words)
+		LanguageTool.POS_list = [str(pos_tags).replace(' ', '') for pos_tags in df[2]]
 
 	@staticmethod
 	def list_to_dict(a_list):
 		a_dict = {a_list[i]: i for i in range(0, len(a_list))}
 		return a_dict
+
+	@staticmethod
+	def pos_tagging(word_list):
+		if not 'LanguageTool.VN_dict' in locals():
+			LanguageTool.create_dictionary()
+
+		result = []
+		for word in word_list:
+			word = word.replace('_', ' ')
+			if word in LanguageTool.VN_dict:
+				pos_tags = LanguageTool.POS_list[LanguageTool.VN_dict[word]]
+				result.append('{}/{}'. format(word.replace(' ', '_'), pos_tags))
+		return result
+
+
 
 # https://stackoverflow.com/questions/3781670/how-to-highlight-text-in-a-tkinter-text-widget
 class CustomText(tk.Text):
@@ -244,3 +320,139 @@ class CustomText(tk.Text):
 			self.mark_set("matchEnd", "%s+%sc" % (index, count.get()))
 			self.tag_add(tag, "matchStart", "matchEnd")
 
+class GanNhanTu():
+
+	def __init__(self, root):
+		self.root = root
+		# textbox cho văn bản đã được tách từ
+		self.input = CustomText(self.root)
+		self.input.bind('<Button-3>', self.open_menu)
+
+		# menu chính
+		self.main_menu = Menu(self.root, tearoff=0)
+		self.main_menu.add_command(label='Dán nhãn từ', command=self.assign)
+
+		# nút mở/lưu file
+		self.open_file_button = tk.Button(self.root, text='Mở file', command=self.open_file)
+		self.save_file_button = tk.Button(self.root, text='Lưu file', command=self.save_file)
+
+		# nút tự dán nhãn từ
+		self.auto_assign_button = tk.Button(self.root, text='Tự động gán nhãn từ', command=self.auto_assign)
+
+		# kết quả
+		self.output = CustomText(self.root)
+
+	def pack(self):
+		self.input.pack()
+		self.open_file_button.pack()
+		self.save_file_button.pack()
+		self.auto_assign_button.pack()
+		self.output.pack()
+
+	def open_file(self):
+		file_name = filedialog.askopenfilename(parent=self.root, initialdir='./', title='Chọn file text', filetypes=(('file text', '*.txt'),))
+		
+		if file_name != '':
+			with open(file_name, encoding='utf-8', mode='r') as file:
+				text = file.read()
+			self.input.delete('1.0', tk.END)
+			self.input.insert('1.0', text)
+
+	def save_file(self):
+		file_name = filedialog.asksaveasfilename(parent=self.root, confirmoverwrite=False, filetypes=(('txt file', '*.txt'),), defaultextension='.txt')
+
+		if file_name != '':
+			with open(file_name, mode='w', encoding='utf-8') as file:
+				file.write(self.output.get('1.0', tk.END))
+
+	def auto_assign(self):
+		text = self.input.get('1.0', tk.END)
+		words = text.split()
+		pos_tagged_words = LanguageTool.pos_tagging(words)
+		self.add_text(self.output, ' '.join(pos_tagged_words))
+
+	def add_text(self, widget, text):
+		widget.insert(tk.END, text + ' ')
+
+	def open_menu(self, event):
+		try:
+			self.main_menu.tk_popup(event.x_root, event.y_root)
+		finally:
+			self.main_menu.grab_release()
+
+	def assign(self):
+		if self.input.tag_ranges("sel"):
+			text = self.input.get("sel.first", "sel.last").strip()
+		else:
+			messagebox.showerror(parent=self.root, title='Chưa chọn từ', message='Xin hãy bôi đen từ cần được dán nhãn')
+			return None
+
+		if text == '':
+			messagebox.showerror(parent=self.root, title='Từ được chọn không hợp lệ', message='Từ được bôi đen không hợp lệ')
+			return None
+
+		top_level = tk.Toplevel(self.root)
+		assign_window = AssignWindow(top_level, text, LanguageTool.pos)
+		assign_window.pack()
+		result = assign_window.show()
+		self.add_text(self.output, result)
+
+class GanThucThe(GanNhanTu):
+	def __init__(self, root):
+		GanNhanTu.__init__(self, root)
+		self.auto_assign_button['text'] = 'Tự động dán thực thể'
+		self.auto_assign_button['state'] = 'disable'
+		self.main_menu.entryconfigure(1, label='Dán nhãn thực thể')
+
+	def assign(self):
+		if self.input.tag_ranges("sel"):
+			text = self.input.get("sel.first", "sel.last").strip()
+		else:
+			messagebox.showerror(parent=self.root, title='Chưa chọn từ', message='Xin hãy bôi đen từ cần được dán nhãn thực thể')
+			return None
+
+		if text == '':
+			messagebox.showerror(parent=self.root, title='Từ được chọn không hợp lệ', message='Từ được bôi đen không hợp lệ')
+			return None
+
+		top_level = tk.Toplevel(self.root)
+		assign_window = AssignWindow(top_level, text, LanguageTool.ne)
+		assign_window.pack()
+		result = assign_window.show()
+		self.add_text(self.output, result)
+
+class AssignWindow():
+	def __init__(self, root, text, val_and_desc):
+		self.root = root
+		self.text = text
+		self.val_and_desc = val_and_desc
+		self.intVars = []
+		self.checkbuttons = []
+		for val, description in self.val_and_desc:
+			var = tk.IntVar()
+			self.intVars.append(var)
+			self.checkbuttons.append(tk.Checkbutton(self.root, text=f'{val}-{description}', variable=var, command=self.on_check))
+		self.display_text = tk.Label(self.root, text=self.text)
+		self.submit_button = tk.Button(self.root, text='Xác nhận', command=self.submit)
+
+	def pack(self):
+		for button in self.checkbuttons:
+			button.pack()
+		self.display_text.pack()
+		self.submit_button.pack()
+
+	def show(self):
+		self.root.deiconify()
+		self.root.wait_window()
+		return self.result
+
+	def on_check(self):
+		val = []
+		for i in range(len(self.intVars)):
+			if self.intVars[i].get() == 1:
+				val.append(self.val_and_desc[i][0])
+		self.display_text['text'] = '{}/{}'.format(self.text, ','.join(val))
+
+	def submit(self):
+		self.result = self.display_text['text']
+		self.root.destroy()
